e95c91cd54db6939d63e1f83d24c4f3a56d87859
ASUS <rodg.13@hotmail.com>
2023-01-07 00:26:36 -0500

    Initial commit1

---
+++ app/src/main/java/com/cyb2d/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cyb2d.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
